2bf47f2ffb954813060c66cc2f9a88742cf110ba
Haiko Hertes <HaikoHertes@users.noreply.github.com>
2019-07-10 12:50:59 +0200

    Update StartupVMsWithAzureAutomation.ps1

--- Azure/Management/StartupVMsWithAzureAutomation.ps1
+++ Azure/Management/StartupVMsWithAzureAutomation.ps1
@@ -2,6 +2,7 @@
     .DESCRIPTION
         This runbooks starts all Azure VMs in all Ressource Groups that have the Tag "AutoStartup" set to "Yes" at the UTC time given in "AutoStartupTime" in the format "HH:mm:ss".
         Attention: This need the Azure Automation modules being updated - take a look on this video: https://www.youtube.com/watch?v=D61XWOeN_w8&t=11s (08:30)
+        Recommondation: Use this Script instead: StartAndStopVMsWithAzureAutomation.ps1 (same Repo)
 
     .NOTES
         AUTHOR: Haiko Hertes
