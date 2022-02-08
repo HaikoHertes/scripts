c6477f075340d745ef2f5bb2cd8db1c108b5503d
Haiko Hertes <github@hertes.net>
2022-02-09 00:03:20 +0100

    First working version

--- Azure/AzureMigrate/GroupAzMigrateServers.ps1
+++ Azure/AzureMigrate/GroupAzMigrateServers.ps1
@@ -1,6 +1,13 @@
 <#
     .DESCRIPTION
-        tbd
+        tbd - no error handling yet
+    .EXAMPLE
+        \GroupAzMigrateServers.ps1 `
+            -SubscriptionId "123456-1c32-7890-a6bd-08154711faa" `
+            -ResourceGroupName "RG-SomeRG" `
+            -AzMigrateProjectName "SomeProject" `
+            -AzMigrateGroupName "SomeGroup" `
+            -ServerNames "SomeServer1","SomeServer2"
     .NOTES
         AUTHOR: Haiko Hertes
                 Microsoft MVP & Azure Architect
@@ -9,43 +16,42 @@
 
 [CmdletBinding()]
 param (
+    
+    # Subscription ID of the Azure Migrate Project to use
     [parameter(Mandatory=$true)]
     [string]
     $SubscriptionId,
     
+    # Resource Group name of the Azure Migrate Project to use
     [parameter(Mandatory=$true)]
     [string]
     $ResourceGroupName,
 
+    # Name of the Azure Migrate Project to use (as defined by the user)
     [parameter(Mandatory=$true)]
     [string]
     $AzMigrateProjectName,
 
+    # Name of the Azure Migrate Assessment group to use - must be preexisting
     [parameter(Mandatory=$true)]
     [string]
     $AzMigrateGroupName,
 
+    # List of Servernames as used in Azure Migrate Assessment / Discovery to be added to the group
     [parameter(Mandatory=$true)]
     [string[]]
     $ServerNames
 )
 
-[string]$SubscriptionId = "26655ce5-1c32-4693-a6bd-505410055faa"
-[string]$ResourceGroupName = "RG-Demos"
-[string]$AzMigrateProjectName = "DemoMigration"
-[string]$AzMigrateGroupName = "NeueGruppe"
-[string[]]$ServerNames = ("SERVER4 - Mail","SERVER5 - Web")
 
 Connect-AzAccount -Subscription $SubscriptionId -WarningAction SilentlyContinue
 Set-AzContext -SubscriptionId $SubscriptionId -WarningAction SilentlyContinue
-
 
 $AssessmentProject = Invoke-AzRestMethod -Method GET -Path "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.Migrate/assessmentProjects?api-version=2019-10-01"
 $InternalAzMigrateProjectName = (($AssessmentProject | Where-Object {($_.Content | ConvertFrom-Json).Value.properties.assessmentSolutionId -like "*/Microsoft.Migrate/MigrateProjects/$AzMigrateProjectName/*"}).Content | ConvertFrom-Json).value.name
 $AllMachines = Invoke-AzRestmethod -Method GET -Path "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.Migrate/assessmentProjects/$InternalAzMigrateProjectName/machines?api-version=2019-10-01"
 $AllMachines = ($AllMachines.Content | ConvertFrom-Json).value
 $RelevantMachines = $AllMachines | Where-Object {$_.Properties.displayName -in $ServerNames}
-
 
 $RESTPayload = "{'properties': {'machines': ['$(([string]($RelevantMachines.id)).Replace(" ","','"))'],'operationType':'Add'}}"
 
@@ -62,16 +68,5 @@ else {
     $Result.Content | ConvertFrom-Json
     ($Result.Content | ConvertFrom-Json).properties
 }
-
-# Call:
-<#
-C:\Users\micro\.vscode\Repos\haikohertes_scripts\scripts\Azure\AzureMigrate\GroupAzMigrateServers.ps1 `
-    -SubscriptionId "26655ce5-1c32-4693-a6bd-505410055faa" `
-    -ResourceGroupName "RG-Demos" `
-    -AzMigrateProjectName "DemoMigration" `
-    -AzMigrateGroupName "NeueGruppe" `
-    -ServerNames "SERVER4 - Mail","SERVER5 - Web"
-
-#>    
 
    
