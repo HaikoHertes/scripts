00d516ea2101d39ef1eccd9584b01e5dfd20f41f
Haiko Hertes <github@hertes.net>
2022-03-09 14:25:18 +0100

    Put some documentation

--- Azure/Networking/DeployNetworkWatcherToCustomRG.ps1
+++ Azure/Networking/DeployNetworkWatcherToCustomRG.ps1
@@ -1,4 +1,48 @@
-ForEach($region in ("westeurope","northeurope","germanywestcentral"))
+$regionsToUse = ("westeurope","northeurope","germanywestcentral")
+$SubscriptionsToUse = "PT_SUB_NAMES_HERE","OR_USE_SUB_IDS"
+$ResourceGroupNameToUse = "RG-NetworkWatcher"
+
+Clear-AzContext -Scope Process
+
+# Login to Azure
+try
 {
-	New-AzNetworkWatcher -Name "NetworkWatcher_$region" -ResourceGroupName "RG-NetworkWatcher" -Location $region
+	Connect-AzAccount -WarningAction SilentlyContinue -ErrorAction Stop
+}
+catch
+{
+	write-output  $_.Exception.message;
+	throw "Error connecting to Azure!"
+}
+Write-Debug "Login successfull!"
+
+$AllSubscriptions = Get-AzSubscription -WarningAction SilentlyContinue
+$SubscriptionObjectsToUse = @()
+ForEach($Sub in $SubscriptionsToUse)
+{
+	$SubscriptionObjectsToUse += $AllSubscriptions | Where-Object {($_.name -eq $Sub) -or ($_.Id -eq $Sub)}
+}
+
+ForEach($SubscriptionObject in $SubscriptionObjectsToUse)
+{
+	try {
+		Set-AzContext -SubscriptionId $SubscriptionObject.Id -WarningAction SilentlyContinue -ErrorAction Stop
+	}
+	catch
+	{
+		write-output  $_.Exception.message;
+		throw "Error switching to Subscription $($SubscriptionObject.Name) ($($SubscriptionObject.Id))!"
+	}
+	Write-Debug "Switching Subscription context successfull!"
+
+	ForEach($region in $regionsToUse)
+	{
+		If((Get-AzResourceGroup -Name $ResourceGroupNameToUse | Measure-Object).Count -lt 1)
+		{
+			Write-Debug "Resource Group $ResourceGroupNameToUse not existing - creating it."
+			New-AzResourceGroup -Name $ResourceGroupNameToUse -Location $region
+		}
+		Write-Debug "Adding Network Watcher for Location $region."
+		New-AzNetworkWatcher -Name "NetworkWatcher_$region" -ResourceGroupName $ResourceGroupNameToUse -Location $region
+	}
 }
