d445f86d95f308f38acf5f95d99b0b10897aeba0
Haiko Hertes <github@hertes.net>
2023-01-21 22:18:54 +0100

    New version

--- Azure/Management/Install-LogAnalyticsAgents.ps1
+++ Azure/Management/Install-LogAnalyticsAgents.ps1
@@ -1,13 +1,13 @@
-Connect-AzAccount -Subscription "9e321b26-01c4-4b44-bb68-3e6ba770b5b5"
-Set-AzContext -Subscription "9e321b26-01c4-4b44-bb68-3e6ba770b5b5"
+Connect-AzAccount -Subscription "SUB_ID_HERE"
+Set-AzContext -Subscription "SUB_ID_HERE"
 
 Set-AzVMExtension -Name AzureMonitorWindowsAgent -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName "RG-AgentTests" -VMName "AMA" -Location "westeurope" -EnableAutomaticUpgrade $true -TypeHandlerVersion  "1.10"
 
 
 
 
-$PublicSettings = @{"workspaceId" = "eb1bc48e-dae1-4a26-86a3-1a1b70a44853"}
-$ProtectedSettings = @{"workspaceKey" = "RrojZIb3EoIy1igxlGLxxy2Vw4YBx6LdHCoYGdJXsrwL6P3Chu55mUGJumCVRlY5QO4NxN4a6xJmmZQ4sWoatQ=="}
+$PublicSettings = @{"workspaceId" = "WORKSPACE_ID_HERE"}
+$ProtectedSettings = @{"workspaceKey" = "WORKSPACE_KEY_HERE"}
 
 Set-AzVMExtension -ExtensionName "MicrosoftMonitoringAgent" `
     -ResourceGroupName "RG-AgentTests" `
